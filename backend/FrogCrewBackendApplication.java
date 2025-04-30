5c309c4322c470dff8c868754def687092a6d107
Alondra Oropeza <alondreza@gmail.com>
2025-04-30 17:15:30 -0500

    Backend continued

---
+++ backend/FrogCrewBackendApplication.java
@@ -0,0 +1,12 @@
+package com.frogcrew.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogcrewBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogcrewBackendApplication.class, args);
+    }
+}
